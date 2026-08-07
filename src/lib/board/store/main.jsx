ad28ce4c02f435db89cb8be2c41ba9b7af6e4252
dynaman216 <dynaman216@users.noreply.github.com>
2026-08-07 14:15:45 -0400

    Rotatation and Drag Limits

--- src/lib/board/store/main.jsx
+++ src/lib/board/store/main.jsx
@@ -29,7 +29,7 @@ const boardState = (set, get) => ({
     translateY: 0,
     scale: 1,
     rotate: 0,
-    limitPan: true,
+    limitPan: false,
   },
   // TODO optimize when same values as before
   updateBoardState: (toUpdate) =>
